bc63d26181a80b4a4c10ef327dfb029460f27fea
weadafrgeds <522191604@qq.com>
2026-03-26 14:39:00 +0800

    Update practoce2.c
    替换

--- demo/03-26-am/practoce2.c
+++ demo/03-26-am/practoce2.c
@@ -1,28 +1,47 @@
 #include <stdio.h>
+
 int main()
 {
-
     int year, month, day;
-    int days = 0; // 总天数
-    int month_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int days = 0;
 
     printf("请输入 年 月 日：");
     scanf("%d %d %d", &year, &month, &day);
 
-    // 1. 累加前面月份的天数
-    for (int i = 0; i < month - 1; i++)
+    switch (month - 1)
     {
-        days += month_days[i];
+    case 11:
+        days += 30; // 11月
+    case 10:
+        days += 31; // 10月
+    case 9:
+        days += 30; // 9月
+    case 8:
+        days += 31; // 8月
+    case 7:
+        days += 31; // 7月
+    case 6:
+        days += 30; // 6月
+    case 5:
+        days += 31; // 5月
+    case 4:
+        days += 30; // 4月
+    case 3:
+        days += 31; // 3月
+    case 2:
+        days += 28; // 2月（平年）
+    case 1:
+        days += 31; // 1月
+    case 0:
+        days += day;
     }
-    // 2. 加上日期
-    days += day;
 
-    // 3. 判断闰年：如果是闰年且月份>2，加1天
+    // 判断闰年：如果是闰年且月份>2，加1天
     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
     {
         if (month > 2)
         {
-            days += 1;
+            days++;
         }
     }
 
